37e709c749b81c9dc3184a1fdeda5027fc3aae8d
orez9 <kacpero.chodorek2@o2.pl>
2021-09-18 20:45:02 +0200

    Changed to .NET 5 from .NET 6

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace ZTMQRKODY
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new General());
