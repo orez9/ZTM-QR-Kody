6adf11c0e836e61e4c6ca0304eaaf8b016c76e14
orez9 <kacpero.chodorek2@o2.pl>
2021-09-18 17:18:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace ZTMQRKODY
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new General());
         }
     }
